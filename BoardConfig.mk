7a4a8364bc2c60ec069fd3ee0f139d9b6d5d9935
Sooraj <sooorajjj@gmail.com>
2014-03-10 22:24:54 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,15 +3,33 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/lge/e450/BoardConfigVendor.mk
 
+#add board platdform 
 TARGET_NO_BOOTLOADER := true
 TARGET_BOARD_PLATFORM := unknown
 TARGET_CPU_ABI := armeabi
 TARGET_BOOTLOADER_BOARD_NAME := e450
 
+
+#check if these are true 
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_CPU_SMP := true
+TARGET_BOOTLOADER_BOARD_NAME := e450
+TARGET_CORTEX_CACHE_LINE_32 := true
+TARGET_USE_SPARROW_BIONIC_OPTIMIZATION := true
+TARGET_ARCH := arm
+TARGET_CPU_VARIANT := cortex-A9
+TARGET_BOOTLOADER_BOARD_NAME := e450
+
+
+#add kernel command line from dmesg and  also confirm kernel size ie partition size and base
 BOARD_KERNEL_CMDLINE := 
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 
+#this is really necessary u hav to specify exact patition size of every partition
 # fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
